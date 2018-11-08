7d63960d1fddaf42f7343c9a37d1833e7b2ec447
zl <zengl65e@visionvera.com>
2018-11-08 17:14:40 +0800

    d

--- models/other_layers/multiclass_loss.py
+++ models/other_layers/multiclass_loss.py
@@ -51,6 +51,6 @@ class MultiClassLoss(nn.Module):
 
         print('batch_conf ',batch_conf.shape)
         print('conf_t_v', conf_t_v.shape)
-        loss_c = F.cross_entropy(conf_t_v,batch_conf,  size_average=False)
+        loss_c = F.mse_loss(conf_t_v,batch_conf,  size_average=False)
 
         return loss_c
