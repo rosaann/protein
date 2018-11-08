7bba03ff0b355bc88fd0a519a89207618323b83a
zl <zengl65e@visionvera.com>
2018-11-08 14:04:06 +0800

    d

--- models/ssd/ssd_z.py
+++ models/ssd/ssd_z.py
@@ -115,6 +115,7 @@ class SSD_Z(nn.Module):
         num_img = x.shape[0]
         output_list = torch.Tensor(num_img, self.num_classes, 1)
         
+        print('source ', sources.shape)
         for i, x in enumerate(sources):
           # for every image
           for conf_net in self.conflist:
@@ -123,7 +124,7 @@ class SSD_Z(nn.Module):
             for c in  conf_net:
                 if i == 0:
                    print('x ',x.shape)
-                conf.append(c(x).permute(0, 2, 3, 1).contiguous())
+                conf.append(c(x).permute(2, 3, 1).contiguous())
 
             conf = torch.cat([o.view(o.size(0), -1) for o in conf], 1)
             if i == 0:
