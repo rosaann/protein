01207eb16bddc7a19939f2cbaa689134b438155e
200445028 <200445028@stud.buct.edu.cn>
2018-11-08 21:36:26 +0800

    d

--- protein.py
+++ protein.py
@@ -57,7 +57,7 @@ class Protein(object):
             self.train_per_epoch(epoch)
     
     def train_per_epoch(self, epoch):
-        epoch_size = int( len(self.train_loader) / 20)
+        epoch_size = int( len(self.train_loader) )
         batch_iterator = iter(self.train_loader)
         train_end = int( epoch_size * 0.8);
         print('epoch_size ', epoch_size, " train_end ", train_end)
