022f8cee74a0c66a9cd27933d778e399b2376e5e
zl <zengl65e@visionvera.com>
2018-11-09 16:48:20 +0800

    d

---
+++ test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  9 16:47:18 2018
+
+@author: zl
+"""
+
+from protein import test_model
+if __name__ == '__main__':
+    test_model()
