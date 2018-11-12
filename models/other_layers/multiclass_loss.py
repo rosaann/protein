ff907cbe65ca86354e6011c2005a255ced90b8bb
200445028 <200445028@stud.buct.edu.cn>
2018-11-12 22:07:30 +0800

    d

--- models/other_layers/multiclass_loss.py
+++ models/other_layers/multiclass_loss.py
@@ -49,8 +49,8 @@ class MultiClassLoss(nn.Module):
             batch_conf = conf_data.view(-1, 1)
             conf_t_v = conf_t.view(-1,1)
 
-        print('batch_conf ',batch_conf.shape)
-        print('conf_t_v', conf_t_v.shape)
+     #   print('batch_conf ',batch_conf.shape)
+     #   print('conf_t_v', conf_t_v.shape)
         loss_c = F.mse_loss(conf_t_v,batch_conf,  size_average=False)
 
         return loss_c
