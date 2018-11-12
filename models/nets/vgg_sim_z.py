88b312f45abcd95a6d322cc75c56fd92114eb128
zl <zengl65e@visionvera.com>
2018-11-12 11:31:26 +0800

    d

--- models/nets/vgg_sim_z.py
+++ models/nets/vgg_sim_z.py
@@ -98,7 +98,7 @@ class VGG_SIM_Z(nn.Module):
         
         layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
         
-        layers += [nn.Linear(194560 x 19, 28)]
+        layers += [nn.Linear(194560 * 19, 28)]
       #  conf_layers += [nn.ReLU(inplace=True)]
         layers += [nn.LogSigmoid()]
         
