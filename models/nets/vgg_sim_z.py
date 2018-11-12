33fbe6354eda7faaeefbd60427f975829f1098e0
zl <zengl65e@visionvera.com>
2018-11-12 11:21:59 +0800

    d

--- models/nets/vgg_sim_z.py
+++ models/nets/vgg_sim_z.py
@@ -38,7 +38,7 @@ class VGG_SIM_Z(nn.Module):
         
         conv2d = nn.Conv2d(in_channels, 128, kernel_size=3, padding=1)
         if batch_norm:
-            layers += [conv2d, nn.BatchNorm2d(64), nn.ReLU(inplace=True)]
+            layers += [conv2d, nn.BatchNorm2d(128), nn.ReLU(inplace=True)]
         else:
             layers += [conv2d, nn.ReLU(inplace=True)]
         in_channels = 128
