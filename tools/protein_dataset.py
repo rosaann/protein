edac5603546c0f8c6d8f405a0f7b4aaf1ae5357b
zl <zengl65e@visionvera.com>
2018-11-13 16:07:42 +0800

    d

--- tools/protein_dataset.py
+++ tools/protein_dataset.py
@@ -9,7 +9,7 @@ Created on Wed Nov  7 14:05:42 2018
 import torch.utils.data as data
 import pandas as pd
 import cv2
-
+import os
 
 class ProteinDataSet(data.Dataset):
     def __init__(self,preproc=None, base_path='../train/', csv_path='../train.csv'):
@@ -18,12 +18,14 @@ class ProteinDataSet(data.Dataset):
         self.base_path = base_path
         self.img_name_tails = [ 'red', 'green', 'blue', 'yellow']
     #    self.img_name_tails = [ 'red', 'green', 'blue']
+        self.idx = 0
         
     def __getitem__(self, index):
         img_id = self.df.get_value(index, 'Id')
         target = self.df.get_value(index, 'Target')
         
         imgs = []
+        
         for tail in self.img_name_tails:
             img_path = self.base_path + img_id + '_' + tail + '.png'
             img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE )
@@ -40,6 +42,9 @@ class ProteinDataSet(data.Dataset):
       #      tar_list.append(tar)
         
     #    print('tar_list in getitem ', tar_list)
+        if target.find(self.idx) >= 0:
+            cv2.imwrite(os.path.join('./data/','{}_.png'.format(self.idx)), img_merg)
+            self.idx += 1
         return img_merg, target
         
     def __len__(self):
