e05f6c45d800dcc89fa47e7facd1962effc4cff8
zl <zengl65e@visionvera.com>
2018-11-17 09:27:30 +0800

    g

--- protein.py
+++ protein.py
@@ -402,12 +402,12 @@ class Protein(object):
         return [self.model.parameters]
         
     def configure_optimizer(self, trainable_param):
-        optimizer = optim.SGD(self.model.parameters(), lr= self.config.v('learn_rate'),
-                        momentum=self.config.v('momentum'), weight_decay= self.config.v('weight_decay'))
+     #   optimizer = optim.SGD(self.model.parameters(), lr= self.config.v('learn_rate'),
+     #                   momentum=self.config.v('momentum'), weight_decay= self.config.v('weight_decay'))
      #   optimizer = optim.RMSprop(trainable_param, lr=self.config.v('learn_rate'),
      #                   momentum=self.config.v('momentum'), alpha=self.config.v('momentum_2'), eps=cfg.EPS, weight_decay=self.config.v('weight_decay'))
-     #   optimizer = optim.Adam(trainable_param, lr=self.config.v('learn_rate'),
-     #                   betas=(self.config.v('momentum'), self.config.v('momentum_2')), eps=self.config.v('eps'), weight_decay=self.config.v('weight_decay'))
+        optimizer = optim.Adam(trainable_param, lr=self.config.v('learn_rate'),
+                        betas=(self.config.v('momentum'), self.config.v('momentum_2')), eps=self.config.v('eps'), weight_decay=self.config.v('weight_decay'))
         
         return optimizer
 
