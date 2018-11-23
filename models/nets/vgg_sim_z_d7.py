792b9c0ed99a12fbdebd9b845f27c7ec43a1558a
zl <zengl65e@visionvera.com>
2018-11-23 16:16:50 +0800

    d

--- models/nets/vgg_sim_z_d7.py
+++ models/nets/vgg_sim_z_d7.py
@@ -171,7 +171,7 @@ class VGG_SIM_Z_D7(nn.Module):
             x = model.line(x)
             #  print('x ', x)
             x = model.sigmoid(x)
-            print('x{0:.4f} '.format(x))
+            print('x{0:.4f} '.format(x.float()))
             return x
             
             
