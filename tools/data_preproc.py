5fbe74472ed28975d6607ec46e22e7650777575b
zl <zengl65e@visionvera.com>
2018-11-26 15:48:42 +0800

    d

--- tools/data_preproc.py
+++ tools/data_preproc.py
@@ -28,9 +28,9 @@ class Data_Preproc(object):
       #  image = image.astype(np.float32)
     #    image -= (103.94, 116.78, 123.68, 100.5)
         transform = transforms.Compose([
-            #    transforms.ToPILImage(),
-            #    transforms.ToTensor(), # range [0, 255] -> [0.0,1.0]
-                transforms.Normalize(mean = (0.5, 0.5, 0.5), std = (0.5, 0.5, 0.5))
+                transforms.ToPILImage(),
+                transforms.ToTensor(), # range [0, 255] -> [0.0,1.0]
+              #  transforms.Normalize(mean = (0.5, 0.5, 0.5), std = (0.5, 0.5, 0.5))
                 ])
         image = transform(image)
         return image
