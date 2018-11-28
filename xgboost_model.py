6310c0af6a21b7a90bd78b20be0cca8ffc1449c3
zl <zengl65e@visionvera.com>
2018-11-28 18:20:03 +0800

    d

--- xgboost_model.py
+++ xgboost_model.py
@@ -33,7 +33,7 @@ def xgboost_train():
                 tar_t[int(tar)] = 1
             tr_hot.append(tar_t)  
             
-        param = {'max_depth':2, 'eta':1, 'silent':1, 'objective':'binary:logistic' }
+        param = {'max_depth':20, 'num_class':27, 'eta':1, 'silent':1, 'objective':'binary:logistic' ,'eval_metric':'mse'}
         num_round = 2
         train_end = int(len(images) * 0.8)
         dtrain = xgb.DMatrix(images[:train_end])
