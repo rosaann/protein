68218e715431342c0fd9bb525313bceacef008ea
zl <zengl65e@visionvera.com>
2018-11-28 16:20:28 +0800

    d

--- random_forest.py
+++ random_forest.py
@@ -64,7 +64,7 @@ def get_test_image_list(pre_dir, df):
         for idx, img_id in enumerate(img_id_list):
             img_path = pre_dir+ '_' + 'green' + '.png'
             img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE )
-            imgs[idx](img)
+            imgs[idx] = img
         
 
         return imgs
