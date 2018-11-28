8ddb949e4ef46cb8edc9550900fa51f7ea91590a
zl <zengl65e@visionvera.com>
2018-11-28 16:52:18 +0800

    d

--- random_forest.py
+++ random_forest.py
@@ -5,7 +5,7 @@ Created on Wed Nov 28 10:16:32 2018
 
 @author: zl
 """
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 import pandas as pd
 import numpy as np
 from tools.protein_dataset import ProteinDataSet
@@ -13,6 +13,8 @@ from tools.data_preproc import Data_Preproc
 import torch.utils.data as data
 from config import Config
 from joblib import dump, load
+from sklearn.grid_search import GridSearchCV
+from sklearn import cross_validation, metrics
 
 def randomForest():
   preproc = Data_Preproc()
@@ -37,13 +39,13 @@ def randomForest():
             
             
     results = []
-    sample_leaf_options = list(range(5, 50, 500))
-    n_estimators_options = list(range(1, 1000, 5))
+    sample_leaf_options = [500, 200, 50]
+    n_estimators_options = [50, 100]
     
     train_end = int(len(images) * 0.8)
     for leaf_size in sample_leaf_options: 
         for n_estimators_size in n_estimators_options: 
-            alg = RandomForestClassifier(min_samples_leaf=leaf_size, n_estimators=n_estimators_size, random_state=50) 
+            alg = RandomForestRegressor(min_samples_leaf=leaf_size, n_estimators=n_estimators_size, random_state=50) 
             alg.fit(images[: train_end], tr_hot[:train_end]) 
             predict = alg.predict(images[train_end : ]) # 用一个三元组，分别记录当前的 min_samples_leaf，n_estimators， 和在测试数据集上的精度 
             results.append((leaf_size, n_estimators_size, (tr_hot[train_end:] == predict).mean())) # 真实结果和预测结果进行比较，计算准确率 
@@ -58,6 +60,7 @@ def get_testimg_imgid_list(df):
         return file_list
 import cv2
 import os
+
 def get_test_image_list(pre_dir, df):
         img_id_list = get_testimg_imgid_list(df)
         imgs = []
@@ -85,5 +88,5 @@ def test():
         print(predict)
         return
 
-#randomForest()
-test()
+randomForest()
+t#est()
