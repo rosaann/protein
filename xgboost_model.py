67a5c991a7ce992f1abd33daa0f91e7c893c7674
zl <zengl65e@visionvera.com>
2018-12-03 13:13:33 +0800

    d

--- xgboost_model.py
+++ xgboost_model.py
@@ -211,7 +211,7 @@ def train_one_model(idinfo_list, class_pair):
     data_img_list = data_img_list.reshape((nsamples,nx*ny))
     print('img shape', data_img_list.shape)   
     
-    Y_enc = MultiLabelBinarizer(class_pair).fit_transform(idinfo_list[2])
+    Y_enc = MultiLabelBinarizer(class_pair).transform(idinfo_list[2])
     for i, y_en in enumerate(Y_enc):
         if i < 1:
             print(y_en)
