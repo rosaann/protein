e6d3d4006ad53b2e3ab3418dfea00ab19ae3e751
zl <zengl65e@visionvera.com>
2018-12-07 23:37:22 +0800

    d

--- tools/data_preproc.py
+++ tools/data_preproc.py
@@ -25,14 +25,14 @@ class Data_Preproc(object):
         interp_methods = [cv2.INTER_LINEAR, cv2.INTER_CUBIC, cv2.INTER_AREA, cv2.INTER_NEAREST, cv2.INTER_LANCZOS4]
         interp_method = interp_methods[random.randrange(5)]
         image = cv2.resize(image, (self.resize[0], self.resize[1]),interpolation=interp_method)
-      #  image = image.astype(np.float32)
+        image = image.astype(np.float32)
     #    image -= (103.94, 116.78, 123.68, 100.5)
-        image.transpose(2, 0, 1)    
+    #    image.transpose(2, 0, 1)    
         transform = transforms.Compose([
              #   transforms.ToPILImage(),
-                transforms.ToTensor(), # range [0, 255] -> [0.0,1.0]
+          #      transforms.ToTensor(), # range [0, 255] -> [0.0,1.0]
               #  transforms.Normalize(mean = (0.5, 0.5, 0.5), std = (0.5, 0.5, 0.5))
                 ])
-        image = transform(image)
+       # image = transform(image)
         return image
       #  return image.transpose(2, 0, 1)
