0aaca3d07057550510a7498cefbb49d2cdf23716
zl <zengl65e@visionvera.com>
2018-12-10 11:25:25 +0800

    d

--- protein.py
+++ protein.py
@@ -161,8 +161,8 @@ class Protein(object):
             
             if self.use_gpu:
                 img = Variable(img.cuda())
-              #  img_to_add = img.unsqueeze(0)
-                img_to_add = img
+                img_to_add = img.unsqueeze(0)
+              #  img_to_add = img
             if i %  banch_num > 0 and i <= (len(test_image_merge_list) - 1):
                 img_list.append(img_to_add)
                 name_list.append(img_name)
