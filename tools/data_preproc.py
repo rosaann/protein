0c97d0c5a3e568f3c44a5ecdaba1c0687a593a30
zl <zengl65e@visionvera.com>
2018-12-10 16:41:00 +0800

    d

--- tools/data_preproc.py
+++ tools/data_preproc.py
@@ -41,7 +41,7 @@ class Data_Preproc(object):
                 transforms.ToTensor(), # range [0, 255] -> [0.0,1.0]
               #  transforms.Normalize(mean = (0.5, 0.5, 0.5), std = (0.5, 0.5, 0.5))
                 ])
-        image = transform(image)
+    #    image = transform(image)
         print('aft img shape ', image.shape)
         return image
       #  return image.transpose(2, 0, 1)
