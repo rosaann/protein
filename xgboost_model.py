4b0708d9e236310d9e1422b2df71441c324e7d83
zl <zengl65e@visionvera.com>
2018-12-13 15:16:10 +0800

    d

--- xgboost_model.py
+++ xgboost_model.py
@@ -203,8 +203,8 @@ def xgboost_train(ifTrain = True, train_to = 29):
     down_sample_list = [0, 0, 0, 0, 0, 0, 0, 500, 0, 0]
     for i_c, c in enumerate( minor_type_class):
         param = param_list[i_c]
-        if i_c != 7:
-            continue
+       # if i_c != 7:
+       #     continue
         x = xgb.XGBClassifier(**param) 
         
         
