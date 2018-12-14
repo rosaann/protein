ae22eeecb9bc1aaee2ab48a28a130eee54b31bcb
zl <zengl65e@visionvera.com>
2018-12-14 17:33:09 +0800

    d

--- class_pair.py
+++ class_pair.py
@@ -20,8 +20,8 @@ param_list = [
              ,'seed':10 ,'max_bin':5, 'random_state':10},
                
                 {'max_depth':6,'silent':0,'n_estimators':5
-             ,'learning_rate':0.3, 'objective':'binary:logistic'
-             ,'nthread':8, 'scale_pos_weight':2
+             ,'learning_rate':0.5, 'objective':'binary:logistic'
+             ,'nthread':8, 'scale_pos_weight':3
              ,'tree_method':'gpu_hist', 'predictor':'gpu_predictor'
              ,'seed':10 ,'max_bin':5},
                 
