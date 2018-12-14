22445e5c66a5f34acaec0a86aa24dc1bcc57a285
200445028 <200445028@stud.buct.edu.cn>
2018-12-14 21:46:45 +0800

    d

--- class_pair.py
+++ class_pair.py
@@ -39,7 +39,7 @@ param_list = [
                
                 {'max_depth':6,'silent':0,'n_estimators':3
              ,'learning_rate':0.3, 'objective':'binary:logistic'
-             ,'nthread':8, 'scale_pos_weight':25
+             ,'nthread':8, 'scale_pos_weight':50
              ,'tree_method':'gpu_hist', 'predictor':'gpu_predictor'
              ,'seed':10 ,'max_bin':5},#15
                 
