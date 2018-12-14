7374d4c09c89b4dd5ad7ddc443f8b94829cccc48
zl <zengl65e@visionvera.com>
2018-12-14 18:05:35 +0800

    d

--- class_pair.py
+++ class_pair.py
@@ -23,7 +23,7 @@ param_list = [
              ,'learning_rate':0.6, 'objective':'binary:logistic'
              ,'nthread':8, 'scale_pos_weight':3
              ,'tree_method':'gpu_hist', 'predictor':'gpu_predictor'
-             ,'seed':10 ,'max_bin':8},#
+             ,'seed':10 ,'max_bin':6},#
                 
                  {'max_depth':6,'silent':0,'n_estimators':5
              ,'learning_rate':0.3, 'objective':'binary:logistic'
