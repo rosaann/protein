0520a821ff87a210d500e7aecff360e918644dfa
zl <zengl65e@visionvera.com>
2018-12-15 12:30:35 +0800

    d

--- class_pair.py
+++ class_pair.py
@@ -19,7 +19,7 @@ param_list = [
              ,'tree_method':'gpu_hist', 'predictor':'gpu_predictor'
              ,'seed':10 ,'max_bin':5, 'random_state':10},
                
-                {'max_depth':20,'silent':0,'n_estimators':5
+                {'max_depth':15,'silent':0,'n_estimators':5
              ,'learning_rate':0.6, 'objective':'binary:logistic'
              ,'nthread':8, 'scale_pos_weight':24
              ,'tree_method':'gpu_hist', 'predictor':'gpu_predictor'
