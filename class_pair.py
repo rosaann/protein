94c56119b9bd0b90a5acde13805374d9d91e59ac
zl <zengl65e@visionvera.com>
2018-12-15 14:44:22 +0800

    d

--- class_pair.py
+++ class_pair.py
@@ -19,7 +19,7 @@ param_list = [
              ,'tree_method':'gpu_hist', 'predictor':'gpu_predictor'
              ,'seed':10 ,'max_bin':5, 'random_state':10},
                
-                {'max_depth':6,'silent':0,'n_estimators':15
+                {'max_depth':6,'silent':0,'n_estimators':5
              ,'learning_rate':0.8, 'objective':'binary:logistic'
              ,'nthread':8, 'scale_pos_weight':25
              ,'tree_method':'gpu_hist', 'predictor':'gpu_predictor'
