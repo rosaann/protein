e95d08a4a1c50b665d67a578250f11c6f0698e67
zl <zengl65e@visionvera.com>
2018-12-15 14:31:56 +0800

    d

--- xgboost_model.py
+++ xgboost_model.py
@@ -297,7 +297,9 @@ def xgboost_train(ifTrain = True, train_to = 29):
         print('img shape', train_img_list.shape)  
         print('start fit ', c)
         print('tar ', train_tar_list)
-        x.fit(train_img_list, train_tar_list)
+        
+       # val_pair = [(img, tar) for img, tar in zip(val_img_list, val_tar_list)]
+        x.fit(train_img_list, train_tar_list, eval_set=[val_img_list, val_tar_list], early_stopping_rounds=3)
         model_path = model_base_path + 'xgboost_model_per_class' + str(c) + '.pkl'        
         x.save_model(model_path)  
         
