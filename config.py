b03b3aeb9077ac2012798bda97760b026127f96b
zl <zengl65e@visionvera.com>
2018-12-26 11:28:17 +0800

    d

--- config.py
+++ config.py
@@ -13,14 +13,14 @@ class Config(object):
        self.data['batch_size_eval'] = 5
        self.data['num_classes'] = 28
        self.data['image_size'] = [300, 300]
-       self.data['learn_rate'] = 0.0006
+       self.data['learn_rate'] = 0.0008
        self.data['momentum'] = 0.9
        self.data['momentum_2'] = 0.99
        self.data['weight_decay'] = 0.0001
        self.data['eps'] = 1e-8
        self.data['lr_gamma'] = 0.98
        self.data['lr_steps'] = [1]
-       self.data['epoches'] = 24
+       self.data['epoches'] = 30
        self.data['out_dir'] = 'outs/'
        self.data['save_per'] = 3
        #self.data['group_id_list']=[[27, 15, 10,9], [8,20,17,24],[26,16,13,12],[22,18,6,14],[11,1,19,3],[4,5,7,23],[2,21,25,0]]
